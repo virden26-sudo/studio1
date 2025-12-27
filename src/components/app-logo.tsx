891f35109f2e342fced97842dbbbb03d15c75908
George “Casper” Virden IV <virden26@gmail.com>
2025-12-27 08:08:02 +0000

    wrong dang it go back keep that a just make it like my image

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -13,7 +13,8 @@ export function AppLogo({ className }: { className?: string }) {
       <svg
         width="40"
         height="40"
-        viewBox="0 0 50 50"
+        viewBox="0 0 40 40"
+        fill="none"
         xmlns="http://www.w3.org/2000/svg"
         preserveAspectRatio="xMidYMid"
       >
@@ -29,18 +30,10 @@ export function AppLogo({ className }: { className?: string }) {
             />
           </linearGradient>
         </defs>
-        <text
-          x="50%"
-          y="50%"
-          dominantBaseline="central"
-          textAnchor="middle"
-          fill="url(#grad1)"
-          fontSize="36"
-          fontFamily="'Great Vibes', cursive"
-          dy=".1em"
-        >
-          A+
-        </text>
+         <path 
+            d="M17.4,31.5l-2.8-8.2h-5.2l-2.8,8.2H2.5L9.8,9.2h4.3l7.3,22.3H17.4z M13.5,19.9l-2.1-6.5c-0.3-1-0.5-2.2-0.8-3.5h-0.1 c-0.2,1.3-0.5,2.5-0.8,3.5l-2.1,6.5H13.5z M25.8,31.5V18.8h-4.3v-3.7h4.3v-4.3h3.7v4.3h4.3v3.7h-4.3v12.7H25.8z"
+            fill="url(#grad1)"
+          />
       </svg>
     </div>
   );
