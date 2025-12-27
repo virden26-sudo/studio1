8a19fab2c53d487e93a62cb25609e92793dc0ee5
George “Casper” Virden IV <virden26@gmail.com>
2025-12-27 07:27:34 +0000

    logo needs fixed

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -1,15 +1,15 @@
+
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 
 export function AppLogo({ className }: { className?: string }) {
   return (
     <div
       className={cn(
-        "rounded-lg flex items-center justify-center w-12 h-12",
+        "rounded-lg flex items-center justify-center w-12 h-12 bg-gradient-to-br from-green-300 to-blue-300",
         className
       )}
     >
-      <Image src="/logo.png" alt="Agenda+ Logo" width={48} height={48} />
+      <span className="font-headline text-2xl text-white">A+</span>
     </div>
   );
 }
