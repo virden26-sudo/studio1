ab4f67c929bdaf8a23e2dc0405969b05f1e61eec
George “Casper” Virden IV <virden26@gmail.com>
2025-12-27 07:45:48 +0000

    not cursive FANCY SCRIPT

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['Pacifico', 'cursive'],
+        headline: ['"Great Vibes"', 'cursive'],
         code: ['monospace'],
       },
       colors: {
