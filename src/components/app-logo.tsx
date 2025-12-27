1f63f9c2b625469810718a9b506ebc62b4689780
George “Casper” Virden IV <virden26@gmail.com>
2025-12-27 08:25:36 +0000

    zoom out

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -14,7 +14,7 @@ export function AppLogo({ className }: { className?: string }) {
       <svg
         width="32"
         height="32"
-        viewBox="0 0 48 48"
+        viewBox="0 0 52 52"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         preserveAspectRatio="xMidYMid"
@@ -34,6 +34,7 @@ export function AppLogo({ className }: { className?: string }) {
          <path 
             d="M21.72 11.68C22.5153 10.7494 23.952 10.6401 24.9393 11.3934C25.9267 12.1467 26.3134 13.4357 25.86 14.56L17.52 35.8H14.16L22.92 13.48C23.16 12.8933 22.9527 12.2134 22.44 11.84C21.9273 11.4667 21.2133 11.4134 20.64 11.72L20.28 11.92L21.72 11.68ZM32.4 20.08H38.28V23.44H32.4V29.56H28.92V23.44H23.04V20.08H28.92V14.1999H32.4V20.08Z"
             fill="url(#grad1)"
+            transform="translate(2, 2)"
           />
       </svg>
     </div>
