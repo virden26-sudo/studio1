3ba077cc51bd7686868abea6fc7a8324ce2afd61
George “Casper” Virden IV <virden26@gmail.com>
2025-12-31 00:43:09 +0000

    fix it

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,8 +4,6 @@
 import {genkit, genkitNext} from '@genkit-ai/next';
 import {googleAI} from '@genkit-ai/google-genai';
 
-export const ai = genkit(
-  genkitNext({
-    plugins: [googleAI()],
-  })
-);
+export const ai = genkit({
+  plugins: [googleAI()],
+});
